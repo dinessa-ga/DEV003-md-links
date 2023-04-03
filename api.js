3fc71e142582a26933292525adf11d0f5c80acb9
= <=>
2023-04-03 01:33:47 -0500

    feat: function readingFile

--- api.js
+++ api.js
@@ -13,24 +13,54 @@ const pathValid = (path) => {
 //ruta absoluta
 const isAbsolute = (direc) => path.isAbsolute(direc);//validar si es absoluta o relativa
 
-console.log(isAbsolute('C:/Users/USER/Documents/DEV003-md-links/readme.md'))
-
-console.log(isAbsolute('./README.md'))
+//console.log(isAbsolute('C:/Users/USER/Documents/DEV003-md-links/readme.md'))
+//console.log(isAbsolute('./README.md'))
 
 //Convertir la ruta a absoluta
 const turnToAbsolute = (absolute) => path.resolve(absolute)
 
-console.log(turnToAbsolute('./README.md')) 
+//console.log(turnToAbsolute('./README.md')) 
   
 //detectar archivo
+const isFile = (path) => {
+  const stats = fs.statSync(path);
+  return stats.isFile()
+}
+//console.log(isFile('C:/Users/USER/Documents/DEV003-md-links/readme.md'))  
+//detectar .md 
+const isFileMd = (file) => path.extname(file) === '.md'; //validar extensión de archivo
+console.log(isFileMd('C:/Users/USER/Documents/DEV003-md-links/folder_files/prueba.md'))
 
+//Leer archivo .md
+const readingFile = (direc) => fs.readFileSync(direc, 'utf8');
 
-  
-//detectar .md"
-  const  isFileMd =  (pathResolved) => {
-    const searchMarkdown = path.parse(pathResolved).ext === '.md'
-    return path.isFileMd(path)
+//console.log(readingFile('C:/Users/USER/Documents/DEV003-md-links/readme.md'))
+
+//crear arreglo href text file
+function findLinks(texto) {
+  const regex = /\[([^\]]+)\]\((http[s]?:\/\/[^\)]+)\)/g;
+  const enlaces = [];
+  let match;
+
+  while ((match = regex.exec(texto)) !== null) {
+    enlaces.push({ 
+      texto: match[1],
+      url: match[2]
+    });
   }
- // console.log(path)
 
+  return enlaces;
+}
+
+//console.log(readFiles('esto es','C:/Users/USER/Documents/DEV003-md-links/readme.md'))
+//console.log(findLinks('esto es','C:/Users/USER/Documents/DEV003-md-links/folder_files/prueba.md'))
+//console.log(readFiles('C:/Users/USER/Documents/DEV003-md-links/readme.md'))
+module.exports = {
+  pathValid,
+  isAbsolute,
+  turnToAbsolute,
+  isFile,
+  isFileMd,
+  readingFile
+}
  
---
+++ folder_files/prueba.md
@@ -0,0 +1,6 @@
+[ ] **Configuración de npm-scripts**
+
+  <details><summary>Links</summary><p>
+
+  * [scripts - Documentación oficial (en inglés)](https://docs.npmjs.com/misc/scripts)
+</p></details>
