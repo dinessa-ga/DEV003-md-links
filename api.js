db2e69da94452d7c291dd7551ee2becda202d0c9
= <=>
2023-04-05 00:02:30 -0500

    Borrando comentarios de console.log

--- api.js
+++ api.js
@@ -8,35 +8,26 @@ const pathValid = (path) => {
    return fs.existsSync(path)
 }
 
-//console.log(pathValid('C:/Users/USER/Documents/DEV003-md-links'));
-//const pathValid1 = (path) => fs.existsSync(path)
-
 //ruta absoluta
 const isAbsolute = (direc) => path.isAbsolute(direc);//validar si es absoluta o relativa
-
-//console.log(isAbsolute('C:/Users/USER/Documents/DEV003-md-links/readme.md'))
-//console.log(isAbsolute('./README.md'))
 
 //Convertir la ruta a absoluta
 const turnToAbsolute = (absolute) => path.resolve(absolute)
 
-//console.log(turnToAbsolute('./README.md')) 
-  
 //detectar archivo
 const isFile = (path) => {
   const stats = fs.statSync(path);
   return stats.isFile()
 }
-//console.log(isFile('C:/Users/USER/Documents/DEV003-md-links/readme.md'))  
 
 //detectar .md 
 const isFileMd = (file) => path.extname(file) === '.md'; 
-//console.log(isFileMd('C:/Users/USER/Documents/DEV003-md-links/folder_files/prueba.md'))
+
 
 //Leer archivo .md
-const readingFile = (direc) => promises.readFile(direc, 'utf8');
-//const linksRead = extract(readingFile)
-//console.log(linksRead)
+const readingFile = (file) => promises.readFile(file, 'utf8');
+
+
 // readingFile('C:/Users/USER/Documents/DEV003-md-links/readme.mz').then((data) => {
 //       console.log(data) //posteriormente esto será mi resolve
 //   }).catch(err => console.log({code: 'ENOENT' }, 'El archivo no puede ser leído'));
@@ -68,18 +59,6 @@ function findLinks(texto, path) {
        console.log(findLinks('folder_files\\prueba.md', texto));
     }).catch(err => console.log(err.message))
 
-
-// const textMD=`[ ] **Configuración de npm-scripts**
-
-// <details><summary>Links</summary><p>
-
-// * [scripts - Documentación oficial (en inglés)](https://docs.npmjs.com/misc/scripts)
-// </p></details>`;
-
-// const links = findLinks(textMD)
-//console.log(links)
-
-//console.log(findLinks('https://docs.npmjs.com/misc/scripts'))
 
 module.exports = {
   pathValid,
